f359e171f1c3e35f955239ac637b69ee928e3055
Commit the frog <40223461+introduce131@users.noreply.github.com>
2024-11-18 15:43:04 +0900

    Update HomeFragment.kt

--- app/src/main/java/com/loveprofessor/recyclingapp/HomeFragment.kt
+++ app/src/main/java/com/loveprofessor/recyclingapp/HomeFragment.kt
@@ -35,9 +35,9 @@ class HomeFragment : Fragment() {
 
         /* 오늘 날짜랑 쓰레기 버리는 날짜랑 비교해서 상단 텍스트에 표시할거임. */
         if(MyApplication.userGarbageday.isNotEmpty()) {
-            //var days:List<String> = MyApplication.userGarbageday.split(",")
-            var days:MutableList<String> = MyApplication.userGarbageday.split(",").toMutableList()
-            days.add("일요일")
+            //var days:MutableList<String> = MyApplication.userGarbageday.split(",").toMutableList()
+            //days.add("일요일")
+            var days:List<String> = MyApplication.userGarbageday.split(",")
             var today = LocalDate.now().dayOfWeek.toString()    // 오늘 날짜에 해당하는 요일을 today에 저장함 (영어)
 
             // 영어로 된 요일을 한글로 매핑 시켜주는 작업을 해야 함
@@ -49,11 +49,8 @@ class HomeFragment : Fragment() {
 
             // 이제 오늘 날짜랑 쓰레기버리는 날짜랑 비교하면 됨
             for(day in days) {
-                if(day == today) {
-                    introText = "${MyApplication.userNickname}님 안녕하세요!\n${today}은 재활용쓰레기\n버리는 날이에요."
-                } else {
-                    introText = "${MyApplication.userNickname}님 안녕하세요!\n오늘도 좋은 하루 되세요!"
-                }
+                if(day == today) introText = "${MyApplication.userNickname}님 안녕하세요!\n${today}은 재활용쓰레기\n버리는 날이에요."
+                else introText = "${MyApplication.userNickname}님 안녕하세요!\n오늘도 좋은 하루 되세요!"
             }
         }
 
